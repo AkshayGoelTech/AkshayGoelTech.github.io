237a1dc7dd34e9706d2f01e4e3e6beac24fa7263
AkshayGoelTech <akshaygoel1@gmail.com>
2016-06-10 21:20:03 +0900

    loader testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,9 +113,12 @@ var $root = $('html, body');
 
 //Loading Screen
 window.onload = function() {
+			var body = document.getElementById('body')
+	 		body.className +=' loaded';
+	 		/*
 	var rnum = Math.floor((Math.random() * 400) + 100);
   	setTimeout(function() {
   		var body = document.getElementById('body')
    		body.className +=' loaded';
-  }, rnum);
+  }, rnum);*/	
 };
