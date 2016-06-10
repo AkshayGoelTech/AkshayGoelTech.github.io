fb312068ded3eb9ac981496c1f3871879f8980c0
AkshayGoelTech <akshaygoel1@gmail.com>
2016-06-10 22:02:32 +0900

    fixed loading screen bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,6 @@
 		var	$window = $(window),
 			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
