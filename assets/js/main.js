6cd4cffdcdeadb399bd621b1458bd59bd6b4ab44
AkshayGoelTech <akshaygoel1@gmail.com>
2016-06-23 21:34:28 +0900

    moved google analytics to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,6 +88,12 @@
 
 })(jQuery);
 
+/*
+Document Functions
+ */
+ $(document).ready(function() {
+ 	
+ })
 //Scroll Animation
 var $root = $('html, body');
 	$('a[href*=#]').click(function() {
@@ -129,10 +135,16 @@ function sendForm() {
 	console.log(ref.toString());
 }
 
-/*
-Document Functions
- */
 
- $(document).ready(function() {
- 	todo
- })
+
+
+ /*
+ Google Analytics
+  */
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-79770649-1', 'auto');
+    ga('send', 'pageview');
