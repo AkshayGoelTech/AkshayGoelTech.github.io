a6746c3f05682e1dc53e842f10dcc28782fe6a22
Akshay Goel <ar-akshay.goel@rakuten.com>
2016-07-29 13:51:43 +0900

    Fixed some more bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,10 +97,6 @@ Document Functions
  		$('#nav').load('topNavBar.html');
  	}
  })
-
- $(window).resize(function(){
-     initPage();
- });
  
  window.onload = function() {
 
@@ -147,9 +143,9 @@ var $root = $('html, body');
 	});
 
  /*
- Google Analytics
-  */
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ 	Google Analytics
+ */
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
